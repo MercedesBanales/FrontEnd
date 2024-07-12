4b75f2c6db7480853bfcc083e56eba01ab5382a7
MercedesBanales <mercibanales@hotmail.com>
2024-07-12 00:34:52 -0300

    Login validation

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,8 +1,10 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Field, Form, Formik } from 'formik';
 import { useRouter } from 'next/navigation';
+import ErrorDialog from './ErrorDialog';
+import * as Yup from 'yup';
 
 const InputPasswordComponent = ({ field, form, ...props }: { field: any, form: any, props: any }) => (
     <input
@@ -24,9 +26,19 @@ const InputEmailComponent = ({ field, form, ...props }: { field: any, form: any,
     />
 );
 
+const LoginSchema = Yup.object().shape({
+    email: Yup.string().email('Invalid email address').required('Email address cannot be empty'),
+    password: Yup.string()
+      .required('Password cannot be empty'),
+  });
 
 export default function LoginForm() {
     const router = useRouter();
+    const [error, setError] = useState(null);
+
+    const closeDialog = () =>{
+        setError(null);
+    }
     
     const handleSubmit = async (values: { email: string, password: string }) => {
         try {
@@ -37,38 +49,47 @@ export default function LoginForm() {
                 },
                 body: JSON.stringify(values),
             });
-
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
+            if (!response.ok) throw new Error('Invalid credentials');
 
             const data = await response.json();
             localStorage.setItem('token', data.token);
-            console.log(localStorage.getItem('token'));
-
+            setError(null);
             router.push('/contacts');
-
-        } catch (error) {
-            console.error('Error during login:', error);
+        } catch (error: any) {
+            setError(error.message);
         }
     };
 
-    return (<Formik
-        initialValues={{
-          email: '',
-          password: '',
-        }}
-        onSubmit={(
-          values: { email: string, password: string }) => {
-          handleSubmit(values);
-        }}
-      >
-        <Form className="flex flex-col items-center w-2/4 gap-8">
-            <div className="flex flex-col w-full gap-4">
-                <Field id="email" name="email" component={InputEmailComponent} />
-                <Field id="password" name="password" component={InputPasswordComponent} />
-            </div>
-            <button className="text-white bg-violet-400 rounded-lg px-12 py-2 w-fit" type="submit">Login</button>
-        </Form>
-    </Formik>)
+    return (
+        <>
+        {error && <ErrorDialog message={error} onClose={closeDialog}/>}
+        <Formik
+            initialValues={{
+            email: '',
+            password: '',
+            }}
+            validationSchema={LoginSchema}
+            onSubmit={(
+            values: { email: string, password: string }) => {
+            handleSubmit(values as { email: string, password: string });
+            }}
+        >
+            {({errors, touched}) => (
+                <Form className="flex flex-col items-center w-2/4 gap-8">
+                <div className="flex flex-col w-full gap-4">
+                    <Field id="email" name="email" component={InputEmailComponent} />
+                    {errors.email && touched.email ? (
+                        <label className="text-red-500 text-sm">{errors.email}</label>
+                    ) : null}
+                    <Field id="password" name="password" component={InputPasswordComponent} />
+                    {errors.password && touched.password ? (
+                        <label className="text-red-500 text-sm">{errors.password}</label>
+                    ) : null}
+                </div>
+                <button className="text-white bg-violet-400 rounded-lg px-12 py-2 w-fit" type="submit">Login</button>
+            </Form>
+            )}
+        </Formik>
+        </>
+    )
 }
