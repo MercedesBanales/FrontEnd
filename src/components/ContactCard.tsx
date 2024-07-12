ac5544fddde2e125f2d892699b8b052cd29c793b
MercedesBanales <mercibanales@hotmail.com>
2024-07-12 11:58:25 -0300

    Contact card UI

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -1,7 +1,25 @@
-export default function ContactCard() {
+import { Contact } from "@/types/Contact";
+import Image from 'next/image';
+
+interface Prop {
+    contact: Contact
+}
+
+export default function ContactCard( { contact }: Prop) {
     return (
-        <div>
-            <p>Contact</p>
+        <div className="flex justify-between rounded-3xl p-4 items-center bg-fuchsia-100 w-5/12">
+            <div className="flex justify-center gap-4 items-center">
+                <img className="rounded-full" src={contact.imagePath} alt="image" />
+                <div className="flex flex-col gap-2">
+                    <h2 className="font-bold text-lg text-black">{contact.name}</h2>
+                    <p className="text-xs text-black">{contact.email}</p>
+                </div>
+            </div>
+            <div>
+                <svg className="w-3 h-3 text-black-80" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 13 5.7-5.326a.909.909 0 0 0 0-1.348L1 1"/>
+                </svg>
+            </div>
         </div>
     );        
 }
--- src/components/ContactsList.tsx
+++ src/components/ContactsList.tsx
@@ -5,20 +5,16 @@ import { RootState } from '@/app/GlobalRedux/store';
 
 export default function ContactsList() {
     const contacts = useSelector((state: RootState) => state.contacts.value);
-    console.log(contacts);
-
-    if (!Array.isArray(contacts)) {
-        console.error('Contacts is not an array:', contacts);
-        return <p>Error: Contacts data is invalid</p>;
-    }
 
     return (
         <>
-        <p>Contacts</p>
             { contacts.length === 0 && <p>No contacts found</p> }
-            { contacts.map((contact) => (
-                <ContactCard key={contact.id} />
-            ))} 
+            <div className="flex flex-wrap w-full gap-4 justify-evenly">
+                { contacts.map((contact) => (
+                    <ContactCard key={contact.id} contact={contact} />
+                ))} 
+            </div>
+           
         </>
        
     );
