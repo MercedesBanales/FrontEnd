f6410747783378d76930f4fe5cb49f1ef5222f5d
MercedesBanales <mercibanales@hotmail.com>
2024-07-14 16:38:28 -0300

    Contacts form flex

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -78,36 +78,36 @@ const ContactForm = forwardRef<FormikProps<ContactValue> | null, Props>( ({ cont
         >
             {({errors, touched, setFieldValue}) => (
                 <Form className="flex flex-col items-center justify-evenly w-full gap-8" encType="multipart/form-data">
-                <div className="flex flex-col flex-wrap w-full items-start h-80 gap-4 py-6 pl-6 pr-8 overflow-auto">
-                    <div className="flex flex-col gap-1 w-1/2">
+                <div className="flex flex-wrap w-full justify-start h-80 gap-2 py-6 pl-6 overflow-scroll">
+                    <div className="flex flex-col gap-1 w-[49%]">
                         <label className="text-black text-base font-medium">Name</label>
                         <Field id="name" name="name" type="name" className="bg-fuchsia-100 px-4 py-3 rounded-lg placeholder-gray-400 text-sm" placeholder={contact?.name}/>
                         {errors.name && touched.name ? (
                             <label className="text-red-500 text-sm w-fit">{errors.name}</label>
                         ) : null}
                     </div>
-                    <div className="flex flex-col gap-1 w-1/2">
+                    <div className="flex flex-col gap-1 w-[49%]">
                         <label className="text-black text-base font-medium">Email</label>
                         <Field id="email" name="email" type="email" className="bg-fuchsia-100 px-4 py-3 rounded-lg placeholder-gray-400 text-sm" placeholder={contact?.email}/>
                         {errors.email && touched.email ? (
                             <label className="text-red-500 text-sm w-fit">{errors.email}</label>
                         ) : null}
                     </div>
-                    <div className="flex flex-col gap-1 w-1/2">
+                    <div className="flex flex-col gap-1 w-[49%]">
                         <label className="text-black text-base font-medium">Address</label>
                         <Field id="address" name="address" className="bg-fuchsia-100 px-4 py-3 rounded-lg placeholder-gray-400 text-sm" placeholder={contact?.address}/>
                         {errors.address && touched.address ? (
                             <label className="text-red-500 text-sm w-fit">{errors.address}</label>
                         ) : null}
                     </div>
-                    <div className="flex flex-col gap-1 w-1/2">
+                    <div className="flex flex-col gap-1 w-[49%]">
                         <label className="text-black text-base font-medium">Phone</label>
                         <Field id="phone" name="phone" className="bg-fuchsia-100 px-4 py-3 rounded-lg placeholder-gray-400 text-sm" placeholder={contact?.phone}/>
                         {errors.phone && touched.phone ? (
                             <label className="text-red-500 text-sm w-fit">{errors.phone}</label>
                         ) : null}
                     </div>
-                    <div className="flex flex-col gap-1 w-1/2">
+                    <div className="flex flex-col gap-1 w-[49%]">
                         <label className="text-black text-base font-medium">Profile Picture</label>
                         <input id="image" name="file" type="file" className="text-sm" 
                             onChange={(event: any) => {
